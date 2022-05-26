fab4580866706ba2cf4a2376cdbfaede0f8f8f5e
madloyer <87596107+madloyer@users.noreply.github.com>
2022-05-27 00:21:55 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -13,10 +13,10 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n@{client.username} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.\n\n • Creator: @{OWNER}\n • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>File-Sharing-Man</a>\n • Owner Repo: @mrismanaziz\n\n👨‍💻 Develoved by @Lunatic0de</b>\n",
+            text=f"<b>Join Group dan Channel Mantapjozz:\n============\n1.@Mantapvids\n2.@mantapjozzarea\n@mantapfilestorage\n============\nJika ingin bot seperti ini silahkan Deploy pada Link dibawah secara gratis\nSource Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>File-Sharing-Man</a>\n============\nOwner Repo: @mrismanaziz\n</b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
+                [[InlineKeyboardButton("Close", callback_data="close")]]
             ),
         )
     elif data == "close":
